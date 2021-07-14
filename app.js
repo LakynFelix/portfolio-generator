12b3873c422a928e0b72160ed5be8d5115f6352a
Lakyn <lakynmphotography@hotmail.com>
2021-07-14 17:51:54 -0400

    added to template

--- app.js
+++ app.js
@@ -129,77 +129,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
-
-
-
-
-
-
-
-
-// const promptProject =  portfolioData => {
-//       .then(projectData => {
-//         portfolioData.projects.push(projectData);
-//         if (projectData.confirmAddProject) {
-//           return promptProject(portfolioData);
-//         } else {
-//           return portfolioData;
-//         }
-//       });
-
-//     console.log(`
-//   =================
-//   Add a New Project
-//   =================
-//   `);
-//     return inquirer.prompt([
-//       {
-//         type: 'input',
-//         name: 'name',
-//         message: 'What is the name of your project?'
-//       },
-//       {
-//         type: 'input',
-//         name: 'description',
-//         message: 'Provide a description of the project (Required)'
-//       },
-//       {
-//         type: 'checkbox',
-//         name: 'languages',
-//         message: 'What did you build this project with? (Check all that apply)',
-//         choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-//       },
-//       {
-//         type: 'input',
-//         name: 'link',
-//         message: 'Enter the GitHub link to your project. (Required)'
-//       },
-//       {
-//         type: 'confirm',
-//         name: 'feature',
-//         message: 'Would you like to feature this project?',
-//         default: false
-//       },
-//       {
-//         type: 'confirm',
-//         name: 'confirmAddProject',
-//         message: 'Would you like to enter another project?',
-//         default: false
-//       }
-//     ]);
-//   };
-  
-
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     console.log(portfolioData);
-//   });
